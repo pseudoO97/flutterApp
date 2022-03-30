01ce7baf11872ffb26492a69dee6229bc6e82491
aboulbarge <aboulbarge.omar@gmail.com>
2022-03-30 02:47:52 +0000

    Bridge Update

--- lib/ui/auth/bridge.dart
+++ lib/ui/auth/bridge.dart
@@ -2,50 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:hilmy/constants/app_colors.dart';
 import 'package:hilmy/routes.dart';
 
-
 class Bridge extends StatelessWidget {
-  const Bridge({ Key? key }) : super(key: key);
+  const Bridge({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kLightBlue,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-            const Text('Êtes-vous : '),
-            const SizedBox(height: 24,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () => { 
-                    Navigator.of(context).pushReplacementNamed(Routes.professionalRegister)
-                    },
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                      color: kBlue
+          const SizedBox(
+            height: 200,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('images/people.png', height: 140, width: 140),
+                    const Text('Êtes-vous : '),
+                    const SizedBox(height: 24),
+                    GestureDetector(
+                      onTap: () => {
+                        Navigator.of(context)
+                            .pushReplacementNamed(Routes.professionalRegister)
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                            color: kViolet),
+                        child: const Text('Professionnel',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: 'Roboto-Black')),
+                      ),
                     ),
-                    child: const Text('Professionnel'),
-                  ),
+                    GestureDetector(
+                      onTap: () => {
+                        Navigator.of(context)
+                            .pushReplacementNamed(Routes.individualRegister)
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        margin: const EdgeInsets.all(16),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                            color: kViolet),
+                        child: const Text('Particulier',
+                            style:
+                                TextStyle(fontSize: 22, color: Colors.white)),
+                      ),
+                    )
+                  ],
                 ),
-                const SizedBox(width: 18,),
-                GestureDetector(
-                  onTap: () => {
-                     Navigator.of(context).pushReplacementNamed(Routes.individualRegister)
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                     decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                      color: kBlue
-                    ),
-                    child: const Text('Particulier'),
-                  ),
-                ),
-              ],
-            )
+              ),
+            ],
+          ),
         ],
       ),
     );
